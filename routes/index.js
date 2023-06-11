5ee0c3e0ced0605600567d0c629d24491b806f6f
Caio-RB <caiorichate@proton.me>
2023-06-11 19:46:36 -0300

    prisma orm installation

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,19 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', {title: 'welcome to the game'});
+  res.render('index.html', {title: "Playground's rule"});
 });
 
 /* GET Sign in and Sign up */
 router.get('/signin', function(req, res, next) {
-  res.render('login.html');
+  res.render('login.html', {title: 'Welcome back'});
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('register.html');
+  res.render('register.html', {title: 'Welcome to the game'});
 });
-
+/*GET dashboard */
+router.get('/dashboard', function(req, res, next){
+  res.render('dashboard.html', {title:'Dashboard'});
+})
 module.exports = router;
